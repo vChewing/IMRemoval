27dbeaf224026e90ba6806f173f27f7f7a4ff654
ShikiSuen <shikisuen@outlook.com>
2023-10-24 10:33:42 +0800

    Start working on IMRemoval MK2.

---
+++ IMRemoval/Sources/IMRemovalApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMRemovalApp.swift
+//  IMRemoval
+//
+//  Created by ShikiSuen on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct IMRemovalApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
