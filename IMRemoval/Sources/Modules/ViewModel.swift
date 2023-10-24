3cef24d52b40d0fff22f5a3258f8944f2fc61053
ShikiSuen <shikisuen@outlook.com>
2023-10-25 01:56:53 +0800

    Repo // Add Privacy Policy & Switch to MIT License.

--- IMRemoval/Sources/Modules/ViewModel.swift
+++ IMRemoval/Sources/Modules/ViewModel.swift
@@ -1,10 +1,6 @@
-// (c) 2023 and onwards The vChewing Project (MIT-NTL License).
+// (c) 2023 and onwards The vChewing Project (MIT License).
 // ====================
 // This code is released under the MIT license (SPDX-License-Identifier: MIT)
-// ... with NTL restriction stating that:
-// No trademark license is granted to use the trade names, trademarks, service
-// marks, or product names of Contributor, except as required to fulfill notice
-// requirements defined in MIT License.
 
 import AppKit
 import FolderMonitor
